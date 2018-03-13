b1b5ce937a0e0802da477615213e2c33b51d6028
UncleWangKing <285296372@qq.com>
2018-03-14 01:16:55 +0800

    serial

---
+++ src/test/java/demo/netty/serial/base/Demo02.java
@@ -0,0 +1,29 @@
+package demo.netty.serial.base;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+/**
+ * nio 序列化
+ */
+public class Demo02 {
+
+	public static void main(String[] args) {
+		int a = 11;
+		int b = 22;
+		
+		// 序列化
+		ByteBuffer buffer = ByteBuffer.allocate(8);
+		buffer.putInt(a);
+		buffer.putInt(b);
+//		buffer.putInt(2);
+		byte[] array = buffer.array();
+		System.out.println(Arrays.toString(buffer.array()));
+		
+		// 反序列化
+		ByteBuffer bb = ByteBuffer.wrap(array);
+		System.out.println("a: " + bb.getInt());
+		System.out.println("b: " + bb.getInt());
+
+	}
+
+}
