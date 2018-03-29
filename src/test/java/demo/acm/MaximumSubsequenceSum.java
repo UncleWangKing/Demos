ebe1acdcc8407e0e0b2b960f0203591e858cc9ed
UncleWangKing <285296372@qq.com>
2018-03-30 01:35:48 +0800

    acm水题开始

---
+++ src/test/java/demo/acm/MaximumSubsequenceSum.java
@@ -0,0 +1,23 @@
+package demo.acm;
+
+public class MaximumSubsequenceSum {
+    public static void main(String[] args) {
+        int[] list = {Integer.MIN_VALUE, Integer.MIN_VALUE, 3};
+//        int[] list = {1, 2, -1, 3, -2};
+        System.out.println(maxSum(list));
+    }
+    private static int maxSum(int[] num){
+        int result = 0;
+        int maxResult = Integer.MIN_VALUE;
+        for (int i = 0; i < num.length; i++) {
+            result += num[i];
+            // 相加之后大于结果值，注意需要考虑全是负数的情况
+            if (result > maxResult) {
+                // 比较是否大于最大值maxResult
+                maxResult = result;
+            }else if (result < 0) // 否则如果相加的结果小于0，并且小于maxResult，这个子串的最大值就确定下来了
+                    result = 0;//重置
+        }
+        return maxResult;
+    }
+}
